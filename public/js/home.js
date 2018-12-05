27b2fbd82e176c2c56580255099a2941fabffbc1
Arthuchaut <arthur.audebrand@gmail.com>
2018-12-05 23:12:35 +0100

    Updated env.json.example

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,6 @@
 'use strict'
 
-let updatePrice = (slide) => {
+let updatePrice = slide => {
     let total = 0;
     let ranges = document.querySelectorAll('input[type="range"]');
     let priceMonitor = document.querySelector('#priceMonitor #price');
